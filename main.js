39426d7cbe3644bc4ccd944ba5bd955857cb4a64
Yuto299 <ota.se.ke@outlook.jp>
2025-04-04 23:14:01 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// function func1(str) {
+//   return str;
+// }
+
+// console.log(func1('func1です'));
+
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1('func1です'));
+
+// const func1 = (str) => {
+//   return `受け取った文字は${str}です`;
+// };
+// console.log(func1('func1です'));
+
+const func2 = (a, b) => {
+  return a + b;
+};
+console.log(func2(1, 2));
